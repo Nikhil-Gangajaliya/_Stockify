702ce430e74dfa4d96b27881d03b7177c0f56e1b
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-02-19 18:18:22 +0530

    updated stock routes with swagger documentaation

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -5,6 +5,46 @@ import { verifyAdmin } from "../middlewares/verifyAdmin.middleware.js";
 const router = Router();
 
 /* ========= ADMIN → TRANSFER ========= */
+
+/**
+ * @openapi
+ * /stock/stores/{storeId}/stock:
+ *   post:
+ *     summary: Transfer Stock From Admin To Store
+ *     tags: [Admin - Stock]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: storeId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Custom Store ID (e.g., str_001)
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - productId
+ *               - qty
+ *             properties:
+ *               productId:
+ *                 type: string
+ *                 example: prd_001
+ *               qty:
+ *                 type: number
+ *                 example: 10
+ *     responses:
+ *       200:
+ *         description: Stock transferred successfully
+ *       400:
+ *         description: Invalid input or insufficient stock
+ *       404:
+ *         description: Store or Product not found
+ */
 router.post("/stores/:storeId/stock", verifyAdmin, transferStockToStore);
 
 export default router;
